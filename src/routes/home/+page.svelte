9c7cfa3d591937781b43cbd3e3ded7348b995fe3
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-04-09 23:08:51 -0300

    feat:Adicionara a funcionalidade de excluir as nostas

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,6 +3,7 @@
     import logo from "../../lib/img/Anote-logo.png"
     import editIcon from "../../lib/img/edit_icon.svg"
     import cancelIcon from "../../lib/img/cancel_icon.svg"
+    import cancelIconWhite from "../../lib/img/cancel_white.svg"
     import addIcon from "../../lib/img/add_icon.svg"
 
     let title_input = ''
@@ -17,6 +18,33 @@
 
     function updatePage(){
         location.reload()
+    }
+
+    const deleteNote = async (idNote) =>{
+
+        let id_note = idNote
+
+        const deleteNoteId = {
+            id: id_note
+        }
+
+        const response = await fetch("http://127.0.0.1:5000/api/delete_note",{
+            method: "DELETE",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(deleteNoteId)
+        });
+
+        const data = await response.json();
+        console.log("Resposta da API:", data)
+        alert(data['message'])
+
+        updatePage()
+
+        if(!response.ok){
+            throw new Error("Ocorreu um erro ao enviar")
+        }
     }
 
     const sendData = async () => {
@@ -48,7 +76,6 @@
 
     const toggleButton = (buttong) =>{
         buttonState = !buttonState;
-        console.log("estado:" + buttong)
 
         if(buttong === 'hidden'){
             buttong = 'none'
@@ -74,8 +101,13 @@
         
         {#each notes as nota}
             <div class="bg-[#ffffff] rounded-xl w-[90%] h-auto mt-5 break-words p-2 transition transform hover:ease-in duration-300 hover:scale-105 md:w-[40%]">
-                <div class="h-[2rem] p-1 rounded-lg font-bold bg-[#7c366c]">
-                    <span class="text-white">{nota.title}</span>
+                <div class="h-[2rem] flex p-1 rounded-lg font-bold bg-[#7c366c]">
+                    <div class="w-[90%]">
+                        <span class="text-white">{nota.title}</span>
+                    </div>
+                    <div class="flex justify-end">
+                        <button on:click={() => deleteNote(nota.id)} class=" rounded-full"><img src="{cancelIconWhite}" alt=""></button>
+                    </div>
                 </div>
 
                 <p class="text-black">{nota.content}</p>
@@ -84,12 +116,12 @@
     </div>
 
     <div class="bg-[#00000081] flex justify-center items-center w-full h-full absolute {hiddenButtonAdd}">
-        <div class="bg-white rounded-xl flex-col w-[80%] h-[20rem]">
+        <div class="bg-white rounded-xl flex-col w-[80%] h-[20rem] md:w-[50%] lg:[30%]">
             <input bind:value={title_input} placeholder="Titulo" class="text-white h-[2rem] w-[95%] m-2 p-2 rounded-lg font-bold bg-[#7c366c]" type="text">
             <div class="bg-[#3b1232] rounded-b-lg flex flex-col h-full">
                 <textarea bind:value={content_input} placeholder="Digite seu texto aqui" class="w-full h-[90%] rounded-b-lg resize-none p-2 r" maxlength="400" name="" id=""></textarea>
                 
-                <div class="flex justify-center items-center  h-[20%] gap-8">
+                <div class="flex justify-center items-center h-[20%] gap-8">
                     <div class="">
                         <button class="bg-red-400 h-[2.5rem] w-[5rem] rounded-md transition transform hover:ease-in duration-300 hover:scale-110">Cancelar</button>
                     </div>
